af77ac53d619eda67b9d7b32e2b2d2c5cfa49a02
Kevin Karma <kevinkarma55@gmail.com>
2019-10-06 02:20:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
 
 function findMatching(name) {
   drivers.filter(function(driver) {
-    return driver.name === name;
+    return driver === name;
   })
 }
